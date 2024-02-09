7b33696f7250bb7c285aea1e5c5e1ed53a5353cf
Salomov-Omadbek <153747643+Salomov-Omadbek@users.noreply.github.com>
2024-02-09 15:34:13 +0500

    Update settings.py

--- news/settings.py
+++ news/settings.py
@@ -127,7 +127,7 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/djangomo/project.com/django/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
